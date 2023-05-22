d56859bceae6faab1eab62bd0d80371ce8310b00
Ashwin Pandey <ashwin.pandey11197@gmail.com>
2023-05-22 19:13:58 +0530

    test the logs

--- index.mjs
+++ index.mjs
@@ -1,1 +1,10 @@
-// for testing this service
+import { log, readLog, info, warn, debug, error, fatal, access, database, system } from "./logger.mjs";
+
+log({level: 'info', message: 'Hello World!'});
+log({level: 'warn', message: 'This is a warning!'});
+
+let issue = new Error();
+issue.message = "There was an error!";
+log({level: 'error', error: issue});
+error(issue);
+// readLog("info");
